6062c42f5e7a02e6bd802e8d0adf3b3bcc8b8c92
Ben Bradley <[ben.bradley@cigna.com|mailto:ben.bradley@cigna.com]>
2014-10-28 12:28:16 -0700

    modified test & hoplines for linux & mac

--- TODO.md
+++ TODO.md
@@ -2,4 +2,5 @@
 
 - Test on Windows box
 - Test on Linux box
+ - Figure out why `done` is happening before `hop` on localhost traceroutes
 - Publish to NPM
--- test/spec.js
+++ test/spec.js
@@ -2,13 +2,14 @@ var Traceroute = require('../'),
   should = require('should');
 
 describe('Traceroute-lite', function () {
+  this.timeout(60000);
 
   it('should be requireable', function () {
     (Traceroute).should.be.a.Function;
   });
 
   it('should return a Traceroute instance', function () {
-    var traceroute = new Traceroute('localhost');
+    var traceroute = new Traceroute('8.8.8.8');
     (traceroute).should.be.an.instanceOf(Traceroute);
     (traceroute).should.have.properties(['host', 'platform', 'profile']);
     (traceroute.start).should.be.a.Function;
@@ -29,11 +30,10 @@ describe('Traceroute-lite', function () {
       });
 
       it('should produce an array of hops', function (done) {
-        var traceroute = new Traceroute('localhost');
+        var traceroute = new Traceroute('8.8.8.8');
         traceroute.on('done', function (err, hops) {
           (err === null).should.equal(true, (err || {}).message);
           (hops).should.be.an.Array;
-          (hops.length).should.equal(1);
           (hops[0]).should.have.properties(['counter', 'ip', 'ms'])
           done();
         });
@@ -45,10 +45,15 @@ describe('Traceroute-lite', function () {
     describe('hop', function () {
 
       it('should emit a hop object', function (done) {
-        var traceroute = new Traceroute('localhost');
+        var traceroute = new Traceroute('8.8.8.8');
+        var aHop;
         traceroute.on('hop', function (hop) {
           (hop).should.be.an.Object;
-          (hop).should.have.properties(['counter', 'ip', 'ms'])
+          (hop).should.have.properties(['counter', 'ip', 'ms']);
+          aHop = hop;
+        });
+        traceroute.on('done', function (err, hops) {
+          (aHop).should.be.an.Object.with.properties(['counter', 'ip', 'ms']);
           done();
         });
         traceroute.start();
@@ -69,11 +74,10 @@ describe('Traceroute-lite', function () {
     });
 
     it('should produce an array of hops', function (done) {
-      var traceroute = new Traceroute('localhost');
+      var traceroute = new Traceroute('8.8.8.8');
       traceroute.start(function (err, hops) {
         (err === null).should.equal(true, (err || {}).message);
         (hops).should.be.an.Array;
-        (hops.length).should.equal(1);
         (hops[0]).should.have.properties(['counter', 'ip', 'ms'])
         done();
       });
